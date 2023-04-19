b5b8dbc49a75494343ee30e2d676413c4738b56b
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-19 11:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let list = document.getElementsByTagName("ul")[0].children;
+console.log(list);
+for(let i =0; i < list.length; i++){
+	if(list[i].id === "level"){
+		console.log(i+1);
+		break;
+	}
+}
